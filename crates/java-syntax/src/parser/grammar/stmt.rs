09f7bc1090c79e20fe2e21354aab5b769cfb133d
cubewhy <qby140326@gmail.com>
2026-04-19 20:26:25 +0800

    feat(java-syntax): jls 14.3 "Local Class and Interface Declarations" support

--- crates/java-syntax/src/parser/grammar/stmt.rs
+++ crates/java-syntax/src/parser/grammar/stmt.rs
@@ -1,3 +1,7 @@
+use crate::ContextualKeyword;
+use crate::grammar::decl::{
+    class_decl_rest, enum_decl_rest, interface_decl_rest, record_decl_rest,
+};
 use crate::kinds::SyntaxKind::*;
 use crate::parser::Parser;
 
@@ -11,28 +15,35 @@ pub fn method_body_or_semicolon(p: &mut Parser) {
     }
 }
 
-// TODO: parse block
+/// Parse a block
+///
+/// https://docs.oracle.com/javase/specs/jls/se26/html/jls-14.html
 pub fn block(p: &mut Parser) {
     let m = p.start();
     p.expect(L_BRACE);
 
     while !p.is_at_end() && !p.at(R_BRACE) {
-        block_statement_stub(p);
+        block_statement(p);
     }
 
     p.expect(R_BRACE);
     m.complete(p, BLOCK);
 }
 
-fn block_statement_stub(p: &mut Parser) {
-    if p.at(L_BRACE) {
-        block(p);
-        return;
-    }
+fn block_statement(p: &mut Parser) {
+    let m = p.start();
 
-    while !p.is_at_end() && !p.at(SEMICOLON) && !p.at(R_BRACE) {
-        p.bump();
+    // Local Class and Interface Declarations
+    // https://docs.oracle.com/javase/specs/jls/se26/html/jls-14.html#jls-14.3
+    if p.at(CLASS_KW) {
+        class_decl_rest(p, m);
+    } else if p.at(ENUM_KW) {
+        enum_decl_rest(p, m);
+    } else if p.at(INTERFACE_KW) {
+        interface_decl_rest(p, m);
+    } else if p.at_contextual_kw(ContextualKeyword::Record) {
+        record_decl_rest(p, m);
+    } else {
+        // TODO: statements
     }
-
-    p.eat(SEMICOLON);
 }
