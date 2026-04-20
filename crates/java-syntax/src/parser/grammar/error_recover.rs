121b6d468016cacedb097259df7238cbfb706c9e
cubewhy <qby140326@gmail.com>
2026-04-20 20:11:03 +0800

    feat(java-syntax): recover var, record, yield in the block handler

--- crates/java-syntax/src/parser/grammar/error_recover.rs
+++ crates/java-syntax/src/parser/grammar/error_recover.rs
@@ -1,13 +1,21 @@
 use crate::{kinds::SyntaxKind, kinds::SyntaxKind::*, parser::Parser};
 
-pub fn recover_until(p: &mut Parser, recovery: &[SyntaxKind]) {
+fn recover_until_internal<F>(p: &mut Parser, recovery: &[SyntaxKind], mut is_contextual: F)
+where
+    F: FnMut(&mut Parser) -> bool,
+{
     while !p.is_at_end() {
-        match p.current() {
-            Some(kind) if recovery.contains(&kind) => break,
-            Some(_) => p.bump(),
-            None => break,
+        if let Some(kind) = p.current()
+            && (recovery.contains(&kind) || is_contextual(p))
+        {
+            break;
         }
+        p.bump();
     }
+}
+
+pub fn recover_until(p: &mut Parser, recovery: &[SyntaxKind]) {
+    recover_until_internal(p, recovery, |_| false);
 }
 
 pub fn recover_until_or_eat(p: &mut Parser, recovery: &[SyntaxKind], eat_if_present: SyntaxKind) {
@@ -89,29 +97,35 @@ pub fn recover_type_argument(p: &mut Parser) {
 }
 
 pub fn recover_block_statement(p: &mut Parser) {
-    // TODO: recover contextual keyword like var, yield, record
-    recover_until_or_eat(
-        p,
-        &[
-            SEMICOLON,
-            R_BRACE,
-            L_BRACE,
-            IF_KW,
-            WHILE_KW,
-            DO_KW,
-            FOR_KW,
-            TRY_KW,
-            SWITCH_KW,
-            SYNCHRONIZED_KW,
-            RETURN_KW,
-            THROW_KW,
-            BREAK_KW,
-            CONTINUE_KW,
-            ASSERT_KW,
-            CLASS_KW,
-            INTERFACE_KW,
-            ENUM_KW,
-        ],
+    let recovery_kinds = &[
         SEMICOLON,
-    );
+        R_BRACE,
+        L_BRACE,
+        IF_KW,
+        WHILE_KW,
+        DO_KW,
+        FOR_KW,
+        TRY_KW,
+        SWITCH_KW,
+        SYNCHRONIZED_KW,
+        RETURN_KW,
+        THROW_KW,
+        BREAK_KW,
+        CONTINUE_KW,
+        ASSERT_KW,
+        CLASS_KW,
+        INTERFACE_KW,
+        ENUM_KW,
+    ];
+
+    let is_contextual_kw = |p: &mut Parser| {
+        let Some(lexeme) = p.current_lexeme() else {
+            return false;
+        };
+        p.at(IDENTIFIER) && matches!(lexeme, "var" | "yield" | "record")
+    };
+
+    recover_until_internal(p, recovery_kinds, is_contextual_kw);
+
+    p.eat(SEMICOLON);
 }
