12f3e8eccac6d1f12e3322decf418cfef5662b93
cubewhy <qby140326@gmail.com>
2026-04-26 09:52:30 +0800

    chore(java-syntax): remove unnecessary comment

--- crates/java-syntax/src/parser/grammar/clauses.rs
+++ crates/java-syntax/src/parser/grammar/clauses.rs
@@ -20,7 +20,7 @@ pub fn throws_clause(p: &mut Parser) {
 
 pub fn extends_clause(p: &mut Parser) {
     let m = p.start();
-    p.expect(EXTENDS_KW); // eat EXTENDS_KW
+    p.expect(EXTENDS_KW);
 
     qualified_name(p);
 
