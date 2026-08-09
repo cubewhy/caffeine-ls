23a1bc6bb44ad3d1279544c076bb0a93aacd4e99
cubewhy <qby140326@gmail.com>
2026-08-09 20:53:13 +0800

    feat(ide): add the with_db api

--- crates/ide/src/lib.rs
+++ crates/ide/src/lib.rs
@@ -1,21 +1,15 @@
-use ide_db::{RootDatabase, base_db::FileChange};
+use std::panic::AssertUnwindSafe;
+
+use ide_db::{
+    RootDatabase,
+    base_db::{FileChange, salsa::Cancelled},
+};
 
 pub use ide_db::line_index::{LineCol, LineIndex};
 
 pub mod delta;
 
-/// Snapshot of [AnalysisHost]
-pub struct Analysis {
-    db: RootDatabase,
-}
-
-impl Analysis {
-    pub fn raw_database(&self) -> &RootDatabase {
-        &self.db
-    }
-}
-
-impl std::panic::UnwindSafe for Analysis {}
+pub type Cancellable<T> = Result<T, Cancelled>;
 
 pub struct AnalysisHost {
     db: RootDatabase,
@@ -50,5 +44,38 @@ impl AnalysisHost {
 impl Default for AnalysisHost {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+/// Snapshot of [AnalysisHost]
+pub struct Analysis {
+    db: RootDatabase,
+}
+
+impl std::panic::UnwindSafe for Analysis {}
+
+impl Analysis {
+    pub fn raw_database(&self) -> &RootDatabase {
+        &self.db
+    }
+
+    /// Performs an operation on the database that may be canceled.
+    ///
+    /// rust-analyzer needs to be able to answer semantic questions about the
+    /// code while the code is being modified. A common problem is that a
+    /// long-running query is being calculated when a new change arrives.
+    ///
+    /// We can't just apply the change immediately: this will cause the pending
+    /// query to see inconsistent state (it will observe an absence of
+    /// repeatable read). So what we do is we **cancel** all pending queries
+    /// before applying the change.
+    ///
+    /// Salsa implements cancellation by unwinding with a special value and
+    /// catching it on the API boundary.
+    fn with_db<F, T>(&self, f: F) -> Cancellable<T>
+    where
+        F: FnOnce(&RootDatabase) -> T + std::panic::UnwindSafe,
+    {
+        Cancelled::catch(AssertUnwindSafe(|| f(&self.db)))
     }
 }
