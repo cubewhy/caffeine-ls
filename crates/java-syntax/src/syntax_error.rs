dbf9600328ca6cfd8f9a7dd89850b53b8c716bec
cubewhy <qby140326@gmail.com>
2026-08-14 14:33:12 +0800

    feat(java-syntax): add SyntaxError struct

---
+++ crates/java-syntax/src/syntax_error.rs
@@ -0,0 +1,33 @@
+use rowan::TextRange;
+
+use crate::{LexicalError, LexicalErrorKind, ParseError, ParseErrorKind};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum SyntaxErrorKind {
+    Lexer(LexicalErrorKind),
+    Parser(ParseErrorKind),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SyntaxError {
+    pub kind: SyntaxErrorKind,
+    pub range: TextRange,
+}
+
+impl From<LexicalError> for SyntaxError {
+    fn from(value: LexicalError) -> Self {
+        Self {
+            kind: SyntaxErrorKind::Lexer(value.kind),
+            range: value.range,
+        }
+    }
+}
+
+impl From<ParseErrorKind> for SyntaxError {
+    fn from(value: ParseError) -> Self {
+        Self {
+            kind: SyntaxErrorKind::Parser(value.kind),
+            range: value.range,
+        }
+    }
+}
